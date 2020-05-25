b8a824a588adfc053e3d0e246085eeca72f0eb93
Bram <brambakx@pm.me>
2020-05-25 14:23:03 +0200

    Init van rest hello world app

---
+++ ENTERPRISE/src/main/java/first/resources/HelloWorldResource.java
@@ -0,0 +1,12 @@
+package first.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/helloworld")
+public class HelloWorldResource {
+    @GET
+    public String getHelloWorld() {
+        return "Hallo!";
+    }
+}
